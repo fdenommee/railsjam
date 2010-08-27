3dad38f4ee2d9682790a4effc4bb9a926ad271b8
Greg Moreno <greg.moreno@gmail.com>
2010-08-26 17:19:10 -0700

    Scaffolded user.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJam::Application.config.secret_token = '196bb8c73304ec3ad3db759910955ed1ebda61eb0489cbe6e3fff40e4bf711bf202286bbc4728b4de4d29f62f982399d69920576526847c269caeacd777c217a'
