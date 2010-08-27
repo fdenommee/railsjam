8294572f369a9fc7276337ea5618d8f8155de731
Greg Moreno <greg.moreno@gmail.com>
2010-08-26 17:48:33 -0700

    Add sex to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100826225204) do
+ActiveRecord::Schema.define(:version => 20100827003506) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "sex"
   end
 
 end
